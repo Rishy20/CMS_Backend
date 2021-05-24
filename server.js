ff69b14eb5636035fbc37b7ae4c070cd07999b59
Rishy20 <58569021+Rishy20@users.noreply.github.com>
2021-05-24 23:28:31 +0530

    Add procfile

--- server.js
+++ server.js
@@ -39,5 +39,9 @@ app.use(reviewerRoutes.routes()).use(reviewerRoutes.allowedMethods());
 
 
 //Setup the port
-app.listen(3000);
+let port = process.env.PORT;
+if (port == null || port == "") {
+    port = 3000;
+}
+app.listen(port);
 console.log("Application is running on port 3000");
