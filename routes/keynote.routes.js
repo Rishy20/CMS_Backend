f586ab38ac7ba00c175ba8b3be0283a45fd3df63
Sandaru Jayasekara <sandarulakshitha1999@gmail.com>
2021-06-28 16:42:43 +0530

    Fix file upload for templates and keynote speakers

--- routes/keynote.routes.js
+++ routes/keynote.routes.js
@@ -1,22 +1,39 @@
+const mime = require("mime-types");
+const fs = require("fs");
+const multer = require("@koa/multer");
+
 //Import Koa router
 const Router = require("@koa/router");
 
 //Import api methods
 const {createKeynote, getKeynote, getKeynotes,updateKeynote,deleteKeynote} =  require('../api/keynote.api');
-const mime = require("mime-types");
-const fs = require("fs");
+
+// Set storage path for files
+let storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const path = 'public/uploads/images/speakers';
+        fs.mkdirSync(path, {recursive: true});
+        return cb(null, path);
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname)
+    }
+})
+
+// Create multer object
+const upload = multer({storage: storage});
 
 const router = new Router({
     //route prefix
     prefix:'/api/v1/keynotes'
 })
 //Get All route
-router.get('/',async ctx=>{
+router.get('/',async ctx=> {
     ctx.body= await getKeynotes() ;
 })
 //Insert route
-router.post('/',async ctx=>{
-    let keynote = ctx.request.body;
+router.post('/', upload.single('file'), async ctx=>{
+    let keynote = JSON.parse(ctx.request.body.values);
     keynote = await createKeynote(keynote);
     //Check if the request is successful
     if(keynote.status==="Fail"){
@@ -38,9 +55,9 @@ router.delete('/:id',async ctx=>{
 
 })
 //Update Route
-router.put('/:id', async ctx=>{
+router.put('/:id', upload.single('file'), async ctx=>{
     const id = ctx.params.id;
-    let keynote = ctx.request.body;
+    let keynote = JSON.parse(ctx.request.body.values);
     keynote = await updateKeynote(id,keynote);
     //Check if the request is successful
     if(keynote.status==="Fail"){
